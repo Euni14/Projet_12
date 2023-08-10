edd6d2946bd024db432c2068db8ab5708895d0b1
Eugénie Manantsoa <eharisolonirina@yahoo.com>
2023-08-11 00:30:37 +0200

    Installation de react rooter, Creation des composants

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Footer from "./containers/footer/Footer";
+import Header from "./containers/header/Header";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          lorem ipsum Learn React
-        </a>
-      </header>
+    <div className="">
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/header/Header.js
@@ -0,0 +1,7 @@
+import "./Header.css";
+
+function Header() {
+  return <div className="header">HEADER</div>;
+}
+
+export default Header;
